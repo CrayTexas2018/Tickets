8a5a1458c3ade1c7dfef135ace7d6b4f41a194c3
Cray Jaeger <cray.jaeger1@gmail.com>
2016-12-25 08:09:47 +0000

    adds sport info view with pricing and team data

--- Tickets/Controllers/SportsController.cs
+++ Tickets/Controllers/SportsController.cs
@@ -11,7 +11,7 @@ namespace Tickets.Controllers
     {
         ApplicationDbContext db;
         // GET: Sports
-        public ActionResult Index(string sport)
+        public ActionResult Info(string sport)
         {
             db = new ApplicationDbContext();
             try
@@ -19,16 +19,30 @@ namespace Tickets.Controllers
                 // Check if sport is in database
                 Sport s = db.Database.SqlQuery<Sport>("EXEC GetSportData @p0", sport).Single();
 
-                // Get content and prices for that sport ID
-                
+                // Get Content for that sport ID over the past month
+                List<Content> c = db.Database.SqlQuery<Content>("EXEC GetContent @p0, @p1, @p2", s.sport_id, DateTime.Now.AddMonths(-1), DateTime.Now).ToList();
 
+                //Get Teams
+                List<Team> t = db.Database.SqlQuery<Team>("EXEC GetTeams @p0", s.sport_id).ToList();
+
+                // Get lowest prices for each team with given sport ID
+                List<Price> p = db.Database.SqlQuery<Price>("EXEC GetLowestSportPrices @p0", s.sport_id).ToList();
+
+                SportViewModel svm = new SportViewModel
+                {
+                    sport = s,
+                    content = c,
+                    prices = p,
+                    teams = t
+                };
+
+                return View(svm);
             }
             catch
             {
-                // The sport does not exist
-            }
-
-            return View();
+                // The sport does not exist, return generic view
+                return RedirectToAction("Index", "Home");
+            }            
         }
     }
 }
--- Tickets/Models/Sport.cs
+++ Tickets/Models/Sport.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Tickets.Models
 {
+    public class SportViewModel
+    {
+        public Sport sport { get; set; }
+
+        public List<Content> content { get; set; }
+
+        public List<Team> teams { get; set; }
+
+        public List<Price> prices { get; set; }
+    }
+
     public class Sport
     {
+        [Key]
         public int sport_id { get; set; }
 
         public string internal_name { get; set; }
@@ -18,5 +31,63 @@ namespace Tickets.Models
         public DateTime created { get; set; }
 
         public DateTime updated { get; set; }
+    }
+
+    public class Content
+    {
+        [Key]
+        public int content_id { get; set; }
+
+        public int sport_id { get; set; }
+        
+        public string head { get; set; }
+
+        public string body { get; set; }
+
+        public bool active { get; set; }
+
+        public DateTime created { get; set; }
+
+        public DateTime updated { get; set; }
+    }
+
+    public class Team
+    {
+        [Key]
+        public int team_id { get; set; }
+
+        public int sport_id { get; set; }
+
+        public string internal_name { get; set; }
+
+        public string web_name { get; set; }
+
+        public string team_name { get; set; }
+
+        public bool active { get; set; }
+
+        public DateTime created { get; set; }
+
+        public DateTime updated { get; set; }
+    }
+
+    public class Price
+    {
+        [Key]
+        public int price_id { get; set; }
+
+        public string user_id { get; set; }
+
+        public int team_id { get; set; }
+
+        public int sport_id { get; set; }
+
+        public decimal price { get; set; }
+
+        public bool active { get; set; }
+
+        public bool created { get; set; }
+
+        public bool updated { get; set; }
     }
 }
